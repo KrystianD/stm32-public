83009b461c0ff2f895d9727f68d9e8c8b9f308ac
Krystian Dużyński <krystekd@gmail.com>
2014-05-23 01:55:10 +0200

    i2c: added support for 32MHz

--- i2c.c
+++ i2c.c
@@ -15,18 +15,18 @@ void i2cInit ()//I2Cobj* obj, I2C_TypeDef* i2c, GPIO_TypeDef* port, int pin)
 	RCC->APB1ENR |= RCC_APB1ENR_I2C1EN;
 	RCC->APB1ENR |= RCC_APB1ENR_I2C2EN;
 	
-	IO_OPEN_DRAIN(SCL);
-	IO_OPEN_DRAIN(SDA);
-	IO_HIGH(SCL);
-	IO_HIGH(SDA);
+	// IO_OPEN_DRAIN(SCL);
+	// IO_OPEN_DRAIN(SDA);
+	// IO_HIGH(SCL);
+	// IO_HIGH(SDA);
 
-	while(IO_IS_LOW(SDA))
-	{
-		IO_LOW(SCL);
-		_delay_us(10);
-		IO_HIGH(SCL);
-		_delay_us(10);
-	}
+	// while(IO_IS_LOW(SDA))
+	// {
+		// IO_LOW(SCL);
+		// _delay_us(10);
+		// IO_HIGH(SCL);
+		// _delay_us(10);
+	// }
 
 	IO_ALT_OPEN_DRAIN(SCL);
 	IO_ALT_OPEN_DRAIN(SDA);
@@ -41,27 +41,16 @@ void i2cInit ()//I2Cobj* obj, I2C_TypeDef* i2c, GPIO_TypeDef* port, int pin)
 	// obj->port = port;
 	// obj->pin = pin;
 
-
-
-	// for(;;)
-	// {
-		// _delay_ms(500);
-		// IO_TOGGLE(SDA);
-		// IO_TOGGLE(SCL);
-	// }
-
-	I2C_OBJ->CR1 = 8;
-	I2C_OBJ->CCR = 0x28;
-	I2C_OBJ->TRISE = 0x09;
+	I2C_OBJ->CR1 &= ~I2C_CR1_PE;
+	// 8
+	// I2C_OBJ->CR1 = 8;
+	// I2C_OBJ->CCR = 0x28;
+	// I2C_OBJ->TRISE = 0x09;
+	// 32
+	I2C_OBJ->CR2 = 32;
+	I2C_OBJ->CCR = 0x28 * 4;
+	I2C_OBJ->TRISE = 32 + 1;
 	I2C_OBJ->CR1 |= I2C_CR1_PE;
-
-	// for(;;)
-	// {
-			// myprintf("%02x %02x %02x\r\n", I2C_OBJ->CR1, I2C_OBJ->SR1, I2C_OBJ->SR2);
-		// // _delay_ms(500);
-		// // IO_TOGGLE(SDA);
-		// // IO_TOGGLE(SCL);
-	// }
 }
 void i2cDeinit ()
 {
