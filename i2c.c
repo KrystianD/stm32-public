529e7d222c56ca3f2776420bd371f6464f204c47
Krystian Dużyński <krystekd@gmail.com>
2014-05-26 21:57:08 +0200

    i2c: added possibility of distinction between multiple I2C interfaces i2c: fixed error of not returning value from i2cWaitUntilStop

--- i2c.c
+++ i2c.c
@@ -1,89 +1,38 @@
 #include "i2c.h"
 
 #include <hardware.h>
+#include <settings.h>
 #include <delay.h>
+#include <myprintf.h>
 
-// typedef struct
-// {
-	// I2C_TypeDef* i2c;
-	// GPIO_TypeDef* port;
-	// int pin;
-// } I2Cobj;
-
-void i2cInit ()//I2Cobj* obj, I2C_TypeDef* i2c, GPIO_TypeDef* port, int pin)
+void i2cInit(I2C_TypeDef* I2C_OBJ)
 {
-	RCC->APB1ENR |= RCC_APB1ENR_I2C1EN;
-	RCC->APB1ENR |= RCC_APB1ENR_I2C2EN;
+	I2C_OBJ->CR1 &= ~I2C_CR1_PE;
+	I2C_OBJ->CR2 = (PCLK1_FREQ / 1000000);
+	I2C_OBJ->CCR = 40 * (PCLK1_FREQ / 8000000);
+	I2C_OBJ->TRISE = (PCLK1_FREQ / 1000000) + 1;
+	I2C_OBJ->CR1 |= I2C_CR1_PE;
+
+	myprintf("cr2 0x%02x\r\nccr = 0x%02x\r\ntrise = 0x%02x\r\n", I2C_OBJ->CR2, I2C_OBJ->CCR, I2C_OBJ->TRISE);
+}
+void i2cDeinit(I2C_TypeDef* I2C_OBJ)
+{
+	I2C_OBJ->CR1 = 0;
 	
 	// IO_OPEN_DRAIN(SCL);
 	// IO_OPEN_DRAIN(SDA);
 	// IO_HIGH(SCL);
 	// IO_HIGH(SDA);
-
-	// while(IO_IS_LOW(SDA))
+	
+	// int i;
+	// for (i = 0; i < 10; i++)
 	// {
-		// IO_LOW(SCL);
-		// _delay_us(10);
-		// IO_HIGH(SCL);
-		// _delay_us(10);
+	// IO_TOGGLE(SCL);
+	// _delay_us (10);
 	// }
-
-	IO_ALT_OPEN_DRAIN(SCL);
-	IO_ALT_OPEN_DRAIN(SDA);
-
-	RCC->APB1RSTR |= RCC_APB1RSTR_I2C1RST;
-	RCC->APB1RSTR |= RCC_APB1RSTR_I2C2RST;
-	_delay_us(1);
-	RCC->APB1RSTR &= ~RCC_APB1RSTR_I2C1RST;
-	RCC->APB1RSTR &= ~RCC_APB1RSTR_I2C2RST;
-	_delay_us(1);
-	// obj->i2c = i2c;
-	// obj->port = port;
-	// obj->pin = pin;
-
-	I2C_OBJ->CR1 &= ~I2C_CR1_PE;
-	// 8
-	// I2C_OBJ->CR1 = 8;
-	// I2C_OBJ->CCR = 0x28;
-	// I2C_OBJ->TRISE = 0x09;
-	// 32
-	I2C_OBJ->CR2 = 32;
-	I2C_OBJ->CCR = 0x28 * 4;
-	I2C_OBJ->TRISE = 32 + 1;
-	I2C_OBJ->CR1 |= I2C_CR1_PE;
-}
-void i2cDeinit ()
-{
-	I2C_OBJ->CR1 = 0;
-
-	IO_OPEN_DRAIN(SCL);
-	IO_OPEN_DRAIN(SDA);
-	IO_HIGH(SCL);
-	IO_HIGH(SDA);
-
-	int i;
-	for (i = 0; i < 10; i++)
-	{
-		IO_TOGGLE(SCL);
-		_delay_us (10);
-	}
-	for (i = 0; i < 10; i++)
-	{
-		IO_TOGGLE(SDA);
-		_delay_us (10);
-	}
-	for (i = 0; i < 10; i++)
-	{
-		IO_TOGGLE(SCL);
-		_delay_us (10);
-	}
-	for (i = 0; i < 10; i++)
-	{
-		IO_TOGGLE(SDA);
-		_delay_us (10);
-	}
-	IO_HIGH(SCL);
-	IO_HIGH(SDA);
+	
+	// IO_HIGH(SCL);
+	// IO_HIGH(SDA);
 }
 
 // #define I2CD(x) myprintf(x"\r\n");
@@ -92,10 +41,10 @@ void i2cDeinit ()
 #define I2CD(x)
 #endif
 
-uint8_t i2cStart (uint8_t addr)
+uint8_t i2cStart(I2C_TypeDef* I2C_OBJ, uint8_t addr)
 {
 	uint32_t t;
-
+	
 	I2C_OBJ->CR1 |= I2C_CR1_START;
 	t = ticks;
 	while (!(I2C_OBJ->SR1 & I2C_SR1_SB))
@@ -107,7 +56,7 @@ uint8_t i2cStart (uint8_t addr)
 			return I2C_ERROR_START;
 		}
 	}
-
+	
 	I2C_OBJ->DR = addr;
 	t = ticks;
 	while (!(I2C_OBJ->SR1 & I2C_SR1_ADDR))
@@ -119,7 +68,7 @@ uint8_t i2cStart (uint8_t addr)
 	uint8_t q = I2C_OBJ->SR2;
 	return I2C_SUCCESS;
 }
-uint8_t i2cWrite (uint8_t data)
+uint8_t i2cWrite(I2C_TypeDef* I2C_OBJ, uint8_t data)
 {
 	I2C_OBJ->DR = data;
 	uint32_t t = ticks;
@@ -131,7 +80,7 @@ uint8_t i2cWrite (uint8_t data)
 	}
 	return I2C_SUCCESS;
 }
-uint8_t i2cRead (uint8_t* data)
+uint8_t i2cRead(I2C_TypeDef* I2C_OBJ, uint8_t* data)
 {
 	uint32_t t = ticks;
 	while (!(I2C_OBJ->SR1 & I2C_SR1_RXNE))
@@ -143,117 +92,124 @@ uint8_t i2cRead (uint8_t* data)
 	*data = I2C_OBJ->DR;
 	return I2C_SUCCESS;
 }
-inline void i2cSetACK ()
+inline void i2cSetACK(I2C_TypeDef* I2C_OBJ)
 {
 	I2C_OBJ->CR1 |= I2C_CR1_ACK;
 }
-inline void i2cSetNACK ()
+inline void i2cSetNACK(I2C_TypeDef* I2C_OBJ)
 {
 	I2C_OBJ->CR1 &= ~I2C_CR1_ACK;
 }
-inline void i2cSetStop ()
+inline void i2cSetStop(I2C_TypeDef* I2C_OBJ)
 {
 	I2C_OBJ->CR1 |= I2C_CR1_STOP;
 }
-inline uint8_t i2cWaitUntilStop ()
+inline uint8_t i2cWaitUntilStop(I2C_TypeDef* I2C_OBJ)
 {
-	while ((I2C_OBJ->SR2 & I2C_SR2_MSL)) I2CD("u1");
-}
-
-uint8_t i2cWriteData (uint8_t addr, uint8_t reg, const uint8_t *buffer, uint8_t cnt)
-{
-	if (i2cStart (addr | 0)) return I2C_ERROR;
-	i2cSetACK ();
-	if (i2cWrite (reg))      return I2C_ERROR;
-	while (cnt--)
+	uint32_t t = ticks;
+	while ((I2C_OBJ->SR2 & I2C_SR2_MSL))
 	{
-		if (i2cWrite (*buffer++)) return I2C_ERROR;
+		I2CD("u1");
+		if (ticks - t > 10)
+			return I2C_ERROR;
 	}
-	i2cSetStop ();
-	if (i2cWaitUntilStop ()) return I2C_ERROR;
 	return I2C_SUCCESS;
 }
-uint8_t i2cReadData (uint8_t addr, uint8_t reg, uint8_t *buffer, uint8_t cnt)
-{
-	if (i2cStart (addr | 0))   return I2C_ERROR;
-	if (i2cWrite (reg))        return I2C_ERROR;
 
+uint8_t i2cWriteData(I2C_TypeDef* I2C_OBJ, uint8_t addr, uint8_t reg, const uint8_t *buffer, uint8_t cnt)
+{
+	if (i2cStart(I2C_OBJ, addr | 0)) return I2C_ERROR;
+	i2cSetACK(I2C_OBJ);
+	if (i2cWrite(I2C_OBJ, reg))      return I2C_ERROR;
+	while (cnt--)
+	{
+		if (i2cWrite(I2C_OBJ, *buffer++)) return I2C_ERROR;
+	}
+	i2cSetStop(I2C_OBJ);
+	if (i2cWaitUntilStop(I2C_OBJ)) return I2C_ERROR;
+	return I2C_SUCCESS;
+}
+uint8_t i2cReadData(I2C_TypeDef* I2C_OBJ, uint8_t addr, uint8_t reg, uint8_t *buffer, uint8_t cnt)
+{
+	if (i2cStart(I2C_OBJ, addr | 0))   return I2C_ERROR;
+	if (i2cWrite(I2C_OBJ, reg))        return I2C_ERROR;
+	
 	if (cnt == 1)
 	{
-		if (i2cStart (addr | 1)) return I2C_ERROR;
-		i2cSetNACK ();
-		i2cSetStop ();
-		if (i2cRead (buffer))    return I2C_ERROR;
-		if (i2cWaitUntilStop ()) return I2C_ERROR;
+		if (i2cStart(I2C_OBJ, addr | 1)) return I2C_ERROR;
+		i2cSetNACK(I2C_OBJ);
+		i2cSetStop(I2C_OBJ);
+		if (i2cRead(I2C_OBJ, buffer))    return I2C_ERROR;
+		if (i2cWaitUntilStop(I2C_OBJ)) return I2C_ERROR;
 		return I2C_SUCCESS;
 	}
 	else
 	{
-		if (i2cStart (addr | 1)) return I2C_ERROR;
-		i2cSetACK ();
+		if (i2cStart(I2C_OBJ, addr | 1)) return I2C_ERROR;
+		i2cSetACK(I2C_OBJ);
 		for (;;)
 		{
 			if (cnt == 2)
 			{
-				if (i2cRead (buffer))    return I2C_ERROR;
-				i2cSetNACK ();
-				i2cSetStop ();
+				if (i2cRead(I2C_OBJ, buffer))    return I2C_ERROR;
+				i2cSetNACK(I2C_OBJ);
+				i2cSetStop(I2C_OBJ);
 				buffer++;
-				if (i2cRead (buffer))    return I2C_ERROR;
-				if (i2cWaitUntilStop ()) return I2C_ERROR;
+				if (i2cRead(I2C_OBJ, buffer))    return I2C_ERROR;
+				if (i2cWaitUntilStop(I2C_OBJ)) return I2C_ERROR;
 				return I2C_SUCCESS;
 			}
 			else
 			{
-				if (i2cRead (buffer))    return I2C_ERROR;
+				if (i2cRead(I2C_OBJ, buffer))    return I2C_ERROR;
 				buffer++;
 				cnt--;
 			}
 		}
 	}
 }
-uint8_t i2cWriteDataNoReg (uint8_t addr, const uint8_t *buffer, uint8_t cnt)
+uint8_t i2cWriteDataNoReg(I2C_TypeDef* I2C_OBJ, uint8_t addr, const uint8_t *buffer, uint8_t cnt)
 {
-	if (i2cStart (addr | 0)) return I2C_ERROR;
-	i2cSetACK ();
+	if (i2cStart(I2C_OBJ, addr | 0)) return I2C_ERROR;
+	i2cSetACK(I2C_OBJ);
 	while (cnt--)
 	{
-		if (i2cWrite (*buffer++)) return I2C_ERROR;
+		if (i2cWrite(I2C_OBJ, *buffer++)) return I2C_ERROR;
 	}
-	i2cSetStop ();
-	if (i2cWaitUntilStop ()) return I2C_ERROR;
+	i2cSetStop(I2C_OBJ);
+	if (i2cWaitUntilStop(I2C_OBJ)) return I2C_ERROR;
 	return I2C_SUCCESS;
 }
-uint8_t i2cReadDataNoReg (uint8_t addr, uint8_t *buffer, uint8_t cnt)
+uint8_t i2cReadDataNoReg(I2C_TypeDef* I2C_OBJ, uint8_t addr, uint8_t *buffer, uint8_t cnt)
 {
 	if (cnt == 1)
 	{
-		if (i2cStart (addr | 1)) return I2C_ERROR;
-		i2cSetNACK ();
-		i2cSetStop ();
-		if (i2cRead (buffer))    return I2C_ERROR;
-		if (i2cWaitUntilStop ()) return I2C_ERROR;
+		if (i2cStart(I2C_OBJ, addr | 1)) return I2C_ERROR;
+		i2cSetNACK(I2C_OBJ);
+		i2cSetStop(I2C_OBJ);
+		if (i2cRead(I2C_OBJ, buffer))    return I2C_ERROR;
+		if (i2cWaitUntilStop(I2C_OBJ)) return I2C_ERROR;
 		return I2C_SUCCESS;
 	}
 	else
 	{
-		if (i2cStart (addr | 1)) return I2C_ERROR;
-		i2cSetACK ();
+		if (i2cStart(I2C_OBJ, addr | 1)) return I2C_ERROR;
+		i2cSetACK(I2C_OBJ);
 		for (;;)
 		{
 			if (cnt == 2)
 			{
-				if (i2cRead (buffer))    return I2C_ERROR;
-				i2cSetNACK ();
-				i2cSetStop ();
+				if (i2cRead(I2C_OBJ, buffer))    return I2C_ERROR;
+				i2cSetNACK(I2C_OBJ);
+				i2cSetStop(I2C_OBJ);
 				buffer++;
-				if (i2cRead (buffer))    return I2C_ERROR;
-				if (i2cWaitUntilStop ()) return I2C_ERROR;
+				if (i2cRead(I2C_OBJ, buffer))    return I2C_ERROR;
+				if (i2cWaitUntilStop(I2C_OBJ)) return I2C_ERROR;
 				return I2C_SUCCESS;
 			}
 			else
 			{
-				if (i2cRead (buffer))    return I2C_ERROR;
+				if (i2cRead(I2C_OBJ, buffer))    return I2C_ERROR;
 				buffer++;
 				cnt--;
 			}
--- i2c.h
+++ i2c.h
@@ -8,21 +8,21 @@
 #define I2C_ERROR_START 2
 #define I2C_ERROR_ADDR  3
 
-void i2cInit();
-void i2cDeinit();
-
-uint8_t i2cStart(uint8_t addr);
+void i2cInit(I2C_TypeDef* I2C_OBJ);
+void i2cDeinit(I2C_TypeDef* I2C_OBJ);
+  
+uint8_t i2cStart(I2C_TypeDef* I2C_OBJ, uint8_t addr);
 // uint8_t i2cStop ();
-uint8_t i2cWrite(uint8_t data);
-uint8_t i2cRead(uint8_t* data);
-void i2cSetACK();
-void i2cSetNACK();
-void i2cSetStop();
-uint8_t i2cWaitUntilStop();
+uint8_t i2cWrite(I2C_TypeDef* I2C_OBJ, uint8_t data);
+uint8_t i2cRead(I2C_TypeDef* I2C_OBJ, uint8_t* data);
+void i2cSetACK(I2C_TypeDef* I2C_OBJ);
+void i2cSetNACK(I2C_TypeDef* I2C_OBJ);
+void i2cSetStop(I2C_TypeDef* I2C_OBJ);
+uint8_t i2cWaitUntilStop(I2C_TypeDef* I2C_OBJ);
 
-uint8_t i2cWriteData(uint8_t addr, uint8_t reg, const uint8_t *buffer, uint8_t cnt);
-uint8_t i2cReadData(uint8_t addr, uint8_t reg, uint8_t *buffer, uint8_t cnt);
-uint8_t i2cWriteDataNoReg(uint8_t addr, const uint8_t *buffer, uint8_t cnt);
-uint8_t i2cReadDataNoReg(uint8_t addr, uint8_t *buffer, uint8_t cnt);
+uint8_t i2cWriteData(I2C_TypeDef* I2C_OBJ, uint8_t addr, uint8_t reg, const uint8_t *buffer, uint8_t cnt);
+uint8_t i2cReadData(I2C_TypeDef* I2C_OBJ, uint8_t addr, uint8_t reg, uint8_t *buffer, uint8_t cnt);
+uint8_t i2cWriteDataNoReg(I2C_TypeDef* I2C_OBJ, uint8_t addr, const uint8_t *buffer, uint8_t cnt);
+uint8_t i2cReadDataNoReg(I2C_TypeDef* I2C_OBJ, uint8_t addr, uint8_t *buffer, uint8_t cnt);
 
 #endif
