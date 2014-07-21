6547e9fd3f4795166fb50ab52a057fa88c46ea9f
Krystian Dużyński <krystekd@gmail.com>
2014-07-21 05:10:36 +0200

    delay: added custom prescaler

--- delay.c
+++ delay.c
@@ -5,9 +5,13 @@
 #include "delay.h"
 #include "settings.h"
 
+#ifndef DELAY_PRESCALER
+#define DELAY_PRESCALER 1
+#endif
+
 void _delay_100us (uint16_t d)
 {
-	DELAY_TIMER->PSC = (F_CPU / 10000) - 1;
+	DELAY_TIMER->PSC = (F_CPU / 10000 / DELAY_PRESCALER) - 1;
 	DELAY_TIMER->EGR = TIM_EGR_UG;
 	while (!(DELAY_TIMER->SR & TIM_SR_UIF));
 	DELAY_TIMER->SR = 0;
@@ -18,7 +22,7 @@ void _delay_100us (uint16_t d)
 }
 void _delay_ms (uint16_t d)
 {
-	DELAY_TIMER->PSC = (F_CPU / 1000) - 1;
+	DELAY_TIMER->PSC = (F_CPU / 1000 / DELAY_PRESCALER) - 1;
 	DELAY_TIMER->EGR = TIM_EGR_UG;
 	while (!(DELAY_TIMER->SR & TIM_SR_UIF));
 	DELAY_TIMER->SR = 0;
@@ -29,7 +33,7 @@ void _delay_ms (uint16_t d)
 }
 void _delay_us (uint16_t d)
 {
-	DELAY_TIMER->PSC = (F_CPU / 1000000) - 1;
+	DELAY_TIMER->PSC = (F_CPU / 1000000 / DELAY_PRESCALER) - 1;
 	DELAY_TIMER->EGR = TIM_EGR_UG;
 	while (!(DELAY_TIMER->SR & TIM_SR_UIF));
 	DELAY_TIMER->SR = 0;
