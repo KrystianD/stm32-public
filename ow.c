f19d11d6f235c90dcef933661489857385f2af95
Krystian Dużyński <krystekd@gmail.com>
2015-01-06 19:02:32 +0100

    ow: added rom search functions

--- ow.c
+++ ow.c
@@ -1,7 +1,8 @@
 #include "ow.h"
 #include <delay.h>
+#include <myprintf.h>
 
-void OW_UART_set9600 ()
+void OW_UART_set9600()
 {
 	IO_ALT_OPEN_DRAIN(OW);
 	IO_HIGH(OW);
@@ -10,7 +11,7 @@ void OW_UART_set9600 ()
 	OW_USART->CR3 = USART_CR3_HDSEL;
 	uint8_t v = USART2->DR;
 }
-void OW_UART_set115200 ()
+void OW_UART_set115200()
 {
 	IO_ALT_OPEN_DRAIN(OW);
 	IO_HIGH(OW);
@@ -19,7 +20,7 @@ void OW_UART_set115200 ()
 	OW_USART->CR3 = USART_CR3_HDSEL;
 	uint8_t v = USART2->DR;
 }
-uint8_t OW_UART_resetPulse ()
+uint8_t OW_UART_resetPulse()
 {
 	uint8_t t = 0;
 	OW_USART->DR = 0xf0;
@@ -28,12 +29,12 @@ uint8_t OW_UART_resetPulse ()
 		t++;
 		if (t == 100)
 			return 2;
-		_delay_us (200);
+		_delay_us(200);
 	}
 	uint8_t v = OW_USART->DR;
-	return v != 0xf0 ? 1 : 0;
+	return v != 0xf0 ? OW_RESET_PRESENT : OW_RESET_NOTPRESENT;
 }
-uint8_t OW_UART_readBit ()
+uint8_t OW_UART_readBit()
 {
 	uint8_t t = 0;
 	OW_USART->DR = 0xff;
@@ -42,12 +43,12 @@ uint8_t OW_UART_readBit ()
 		t++;
 		if (t == 100)
 			return 0;
-		_delay_us (2);
+		_delay_us(2);
 	}
 	uint8_t v = OW_USART->DR;
 	return v == 0xff ? 1 : 0;
 }
-uint8_t OW_UART_writeBit (uint8_t b)
+uint8_t OW_UART_writeBit(uint8_t b)
 {
 	uint8_t t = 0;
 	OW_USART->DR = b ? 0xff : 0x00;
@@ -56,34 +57,34 @@ uint8_t OW_UART_writeBit (uint8_t b)
 		t++;
 		if (t == 100)
 			return 0;
-		_delay_us (2);
+		_delay_us(2);
 	}
 	uint8_t v = OW_USART->DR;
 }
-void OW_UART_writeByte (uint8_t byte)
+void OW_UART_writeByte(uint8_t byte)
 {
 	uint8_t i;
 	for (i = 0; i < 8; i++)
 	{
-		OW_UART_writeBit (byte & 0x01);
+		OW_UART_writeBit(byte & 0x01);
 		byte >>= 1;
 	}
 }
-uint8_t OW_UART_readByte ()
+uint8_t OW_UART_readByte()
 {
 	uint8_t i, v = 0;
 	for (i = 0; i < 8; i++)
 	{
 		v >>= 1;
-		if (OW_UART_readBit ())
+		if (OW_UART_readBit())
 			v |= 0x80;
 	}
 	return v;
 }
-uint8_t OW_crc8_update (uint8_t crc, char d)
+uint8_t OW_crc8_update(uint8_t crc, char d)
 {
 	uint8_t j;
-
+	
 	crc ^= d;
 	for (j = 0; j < 8; j++)
 	{
@@ -98,4 +99,92 @@ uint8_t OW_crc8_update (uint8_t crc, char d)
 		}
 	}
 	return crc;
+}
+
+void OW_UART_romSearchInit(struct TOWUARTSearchBuffer* data)
+{
+	data->i = 0;
+	data->j = 0;
+	data->collisionIdx = -1;
+	data->prevCollisionIdx = -1;
+
+	OW_UART_set9600();
+	OW_UART_resetPulse();
+	OW_UART_set115200();
+	OW_UART_writeByte(OW_SEARCH_ROM);
+}
+uint8_t OW_UART_romSearch(struct TOWUARTSearchBuffer* data)
+{
+	uint8_t destBit;
+	uint8_t b = OW_UART_readBit();
+	uint8_t bc = OW_UART_readBit();
+	// myprintf("b: %d bc: %d\r\n", b, bc);
+	
+	uint8_t idx = data->i * 8 + data->j;
+	
+	if (b == 0 && bc == 0)
+	{
+		if (idx > data->collisionIdx)
+		{
+			data->collisionIdx = idx;
+			destBit = 0;
+		}
+		else if (idx < data->collisionIdx)
+		{
+			destBit = (data->rom[data->i] & (1 << data->j)) ? 1 : 0;
+		}
+		else
+		{
+			destBit = 1;
+			if (data->collisionIdx != data->prevCollisionIdx)
+				data->collisionIdx = data->prevCollisionIdx;
+			else
+				data->collisionIdx = -1;
+			data->prevCollisionIdx = -1;
+		}
+		if (destBit == 0)
+		{
+			data->prevCollisionIdx = idx;
+		}
+	}
+	else if (b == 1 && bc == 1)
+	{
+		return OW_ROMSEARCH_NOSENSORS;
+	}
+	else
+	{
+		destBit = b;
+	}
+	// myprintf("go: %d i: %d j: %d idx: %d c: %d pc: %d", destBit, data->i, data->j, idx, data->collisionIdx, data->prevCollisionIdx);
+	OW_UART_writeBit(destBit);
+	if (destBit)
+		data->rom[data->i] |= (1 << data->j);
+	else
+		data->rom[data->i] &= ~(1 << data->j);
+		
+	// myprintf("\n");
+	
+	data->j++;
+	if (data->j == 8)
+	{
+		data->i++;
+		data->j = 0;
+		if (data->i == 8)
+		{
+			data->i = data->j = 0;
+			if (data->collisionIdx == -1)
+			{
+				return OW_ROMSEARCH_ROMEND;
+			}
+			else
+			{
+				OW_UART_set9600();
+				OW_UART_resetPulse();
+				OW_UART_set115200();
+				OW_UART_writeByte(OW_SEARCH_ROM);
+				return OW_ROMSEARCH_ROM;
+			}
+		}
+	}
+	return OW_ROMSEARCH_CONTINUE;
 }
--- ow.h
+++ ow.h
@@ -18,18 +18,47 @@
 #	error OW_USART_APBEN must be defined
 #endif
 
-static inline void OW_UART_init ()
+#define OW_SEARCH_ROM   0xf0
+#define OW_READ_ROM     0x33
+#define OW_MATCH_ROM    0x55
+#define OW_SKIP_ROM     0xcc
+#define OW_ALARM_SEARCH 0xec
+#define OW_CONVERT_T    0x44
+#define OW_WRITE_SCRATCHPAD 0x4e
+#define OW_READ_SCRATCHPAD  0xbe
+#define OW_COPY_SCRATCHPAD  0x48
+#define OW_RECALL_E2        0xe8
+#define OW_READ_SUPPLY      0xb4
+
+static inline void OW_UART_init()
 {
 	RCC->OW_USART_APB |= OW_USART_APBEN;
 }
 
-void OW_UART_set9600 ();
-void OW_UART_set115200 ();
-uint8_t OW_UART_resetPulse ();
-uint8_t OW_UART_readBit ();
-uint8_t OW_UART_writeBit (uint8_t b);
-void OW_UART_writeByte (uint8_t byte);
-uint8_t OW_UART_readByte ();
-uint8_t OW_crc8_update (uint8_t crc, char d);
+#define OW_RESET_NOTPRESENT 0
+#define OW_RESET_PRESENT    1
+
+void OW_UART_set9600();
+void OW_UART_set115200();
+uint8_t OW_UART_resetPulse();
+uint8_t OW_UART_readBit();
+uint8_t OW_UART_writeBit(uint8_t b);
+void OW_UART_writeByte(uint8_t byte);
+uint8_t OW_UART_readByte();
+uint8_t OW_crc8_update(uint8_t crc, char d);
+
+#define OW_ROMSEARCH_ROM       0
+#define OW_ROMSEARCH_CONTINUE  1
+#define OW_ROMSEARCH_ROMEND    2
+#define OW_ROMSEARCH_NOSENSORS 3
+
+struct TOWUARTSearchBuffer
+{
+	uint8_t i, j;
+	int8_t collisionIdx, prevCollisionIdx;
+	uint8_t rom[8];
+};
+void OW_UART_romSearchInit(struct TOWUARTSearchBuffer* data);
+uint8_t OW_UART_romSearch(struct TOWUARTSearchBuffer* data);
 
 #endif
